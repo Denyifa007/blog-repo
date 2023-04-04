ba252dd32c06b655e8d72c8015641ba4ea05542c
Oyeindenyifa Diegbegha <121164225+Denyifa007@users.noreply.github.com>
2023-04-04 16:35:04 +0100

    commit

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import HomePageView, DetailPageView, PostCreateView
+from .views import LandingPageView, HomePageView, DetailPageView, PostCreateView, PostUpdateView, PostDeleteView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns= [ 
-    path('', HomePageView.as_view(), name='home'),
+    path('', LandingPageView.as_view(), name = 'index'),             
+    path('home/', HomePageView.as_view(), name='home'),
     path('post/<int:id>/', DetailPageView.as_view(), name='post_detail'),
     path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('register/', views.register, name='register'),
+
 ]
--- MyBlog/views.py
+++ MyBlog/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
 # for tomorrow's class
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
-class HomePageView(TemplateView):
+
+class HomePageView( LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
     def get(self, request):
@@ -15,7 +20,7 @@ class HomePageView(TemplateView):
         context = {'posts': posts}
         return render(request, self.template_name, context)
 
-class DetailPageView(DetailView):
+class DetailPageView( LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
 
@@ -25,8 +30,37 @@ class DetailPageView(DetailView):
     
     
 # for tomorrow
-class PostCreateView(CreateView):
+class PostCreateView( LoginRequiredMixin, CreateView):
     model = Post
     fields = ['author', 'title', 'description', 'published_date']
     template_name = 'post_create.html' # Add this line
     success_url = reverse_lazy('home')
+    
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['author', 'title', 'description', 'published_date']
+    template_name = 'post_edit.html' # Add this line
+    success_url = reverse_lazy('home')
+    
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('home')
+    template_name = 'post_confirm_delete.html'
+    
+class LandingPageView(TemplateView):
+
+    template_name = "index.html"
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {user}')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
